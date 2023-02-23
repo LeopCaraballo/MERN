5262972e3ae9f98e8330dcfe1ef48cd3fc6a947b
Leopoldo Caraballo <leopcaraballo@gmail.com>
2023-02-23 13:20:16 -0500

    Holagordis

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
 const express = require('express')
 const app = express()
 
-const archivoBD = require('./conexion')
-
-
-app.get('/', (req, res) => {
-    res.end('Hello World!')
-    
-})
-
-
-app.listen(5000,function(){
-    console.log('server is hola running')
-})
+// HOLA MI GORDIS
